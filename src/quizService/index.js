f684cdd998185f163be1ee5630d8e4746caf9015
Stephane Van Pe <stephanevanpe@gmail.com>
2020-11-19 11:22:09 +0100

    first commit

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "Virgin Trains, Virgin Atlantic et Virgin Racing, sont toutes des entreprises appartenant à quel entrepreneur célèbre?   ",
+    answers: ["Richard Branson", "Alan Sugar", "Donald Trump", "Bill Gates"],
+    correct: "Richard Branson",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Où se trouve la gare "LlanfairÂpwllgwyngyllÂgogeryÂchwyrnÂdrobwllÂllanÂtysilioÂgogoÂgoch"?',
+    answers: ["Wales", "Moldova", "Czech Republic", "Denmark"],
+    correct: "Wales",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Avec quelle entreprise Valve a-t-il coopéré à la création du Vive?",
+    answers: ["HTC", "Oculus", "Google", "Razer"],
+    correct: "HTC",
+    questionId: "267908"
+  },
+  {
+    question: "Quel est le nom des parents de Batman?",
+    answers: [
+      "Thomas & Martha",
+      "Joey & Jackie",
+      "Jason & Sarah",
+      "Todd & Mira"
+    ],
+    correct: "Thomas & Martha",
+    questionId: "333247"
+  },
+  {
+    question: "Quel est le nom de famille le plus courant du Pays de Galles?",
+    answers: ["Jones", "Williams", "Davies", "Evans"],
+    correct: "Jones",
+    questionId: "496293"
+  },
+  {
+    question:
+      "Quel était le nom de l'équipe de lutte professionnelle de la WWF composée des lutteurs Axe et Smash?",
+    answers: [
+      "Demolition",
+      "The Dream Team",
+      "The Bushwhackers",
+      "The British Bulldogs"
+    ],
+    correct: "Demolition",
+    questionId: "588909"
+  },
+  {
+    question:
+      "Quel nom représente la lettre «M» dans l'alphabet phonétique de l'OTAN?",
+    answers: ["Mike", "Matthew", "Mark", "Max"],
+    correct: "Mike",
+    questionId: "648452"
+  },
+  {
+    question: "Quel est le premier livre de l'Ancien Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Dans la franchise de jeux vidéo Kingdom Hearts, le protagoniste principal porte une arme de quelle forme?",
+    answers: ["Cléf", "Epée", "Stylo", "Telephone portable"],
+    correct: "Cléf",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Quel jouet le plus vendu de 1983 a provoqué l'hystérie, entraînant des émeutes dans les magasins?",
+    answers: [
+      "Poupée cabbage",
+      "Transformers",
+      "Wini l'ourson",
+      "Rubik cube"
+    ],
+    correct: "Poupée cabbage",
+    questionId: "98390"
+  },
+  {
+    question: "Sur quoi marche un funambule?",
+    answers: ["Une corde", "Glasse brisée", "Balls", "La lune"],
+    correct: "Une corde",
+    questionId: "1071006"
+  },
+  {
+    question: "Dans le passé, qu'est-ce qu'un gentleman garderait dans sa poche?",
+    answers: ["Montre", "Monnaie", "Clefs", "Ordi"],
+    correct: "Montre",
+    questionId: "1174154"
+  },
+  {
+    question: "La zone 51 est située dans quel état américain?",
+    answers: ["Nevada", "Arizona", "New Mexico", "Utah"],
+    correct: "Nevada",
+    questionId: "1226535"
+  },
+  {
+    question: "Comment dire au revoir en espagnol?",
+    answers: ["Adios", " Hola", "Au Revoir", "By"],
+    correct: "Adios",
+    questionId: "1310938"
+  },
+  {
+    question: "Quel est le plus grand organe du corps humain?",
+    answers: ["Peau", "Coeur", "Gros intestin", "Foie"],
+    correct: "Peau",
+    questionId: "1436873"
+  },
+  {
+    question: "Quel signe du zodiaque est représenté par le crabe?",
+    answers: ["Cancer", "Balance", "Vierge", "Sagittaire"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "Sur un jeu de fléchettes, quel numéro est directement en face du n ° 1?",
+    answers: ["19", "20", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "Que signifie le 'S' dans l'abréviation SIM, comme dans la carte SIM? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Quel mot représente la lettre «T» dans l'alphabet phonétique de l'OTAN?",
+    answers: ["Tango", "Target", "Taxi", "Turkey"],
+    correct: "Tango",
+    questionId: "1822532"
+  },
+  {
+    question: "Quel président américain apparaît sur un billet d'un dollar?",
+    answers: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Benjamin Franklin"
+    ],
+    correct: "George Washington",
+    questionId: "195075"
+  },
+  {
+    question: 'Qu est-ce que "dabbing"?',
+    answers: ["Une danse", "Une procedure médicale", "Un sport", "Un language"],
+    correct: "Une danse",
+    questionId: "2019778"
+  },
+  {
+    question: "Quel est le nom du nouvel an juif?",
+    answers: ["Rosh Hashanah", "Elul", "New Year", "Succoss"],
+    correct: "Rosh Hashanah",
+    questionId: "2134343"
+  },
+  {
+    question: "Lequel des jeux de rythme suivants a été créé par Harmonix?",
+    answers: [
+      "Rock Band",
+      "Meat Beat Mania",
+      "Guitar Hero Live",
+      "Dance Dance Revolution"
+    ],
+    correct: "Rock Band",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Quel type d'animal était Harambe, qui a été abattu après qu'un enfant soit tombé dans son enclos au zoo de Cincinnati?",
+    answers: ["Gorille", "Tigre", "Panda", "Crocodile"],
+    correct: "Gorille",
+    questionId: "2379831"
+  },
+  {
+    question: "Red Vines est une marque de quel type de bonbons?",
+    answers: ["Réglisse", "Sucette", "Chocolat", "Bubblegum"],
+    correct: "Réglisse",
+    questionId: "2426418"
+  },
+  {
+    question: "Quel est le surnom de l'État américain de Californie?",
+    answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: "Qu'est-ce qui est exposé au musée Madame Tussaud's à Londres?",
+    answers: [
+      "Sculptures de cire",
+      "Vêtements de créateurs",
+      "Bobines de films inédites",
+      "Voitures anciennes"
+    ],
+    correct: "Sculptures de cire",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Quel a été le surnom donné au Hughes H-4 Hercules, un hydravion de transport lourd qui a volé en 1947?",
+    answers: ["Spruce Goose", "Noah's Ark", "Fat Man", "Trojan Horse"],
+    correct: "Spruce Goose",
+    questionId: "2796884"
+  },
+  {
+    question: "Laquelle de ces couleurs ne figure PAS dans le logo de Google?",
+    answers: ["Rose", "Jaune", "Bleu", "Vert"],
+    correct: "Rose",
+    questionId: "2838900"
+  },
+  {
+    question: 'Quel est le mot français pour "hat"?',
+    answers: ["Chapeau", "Bonnet", " Echarpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Cinq dollars valent combien de nickel?",
+    answers: ["100", "50", "25", "69"],
+    correct: "100",
+    questionId: "3096579"
+  },
+  {
+    question: "Qui est représenté sur le billet de cent dollars américains?",
+    answers: [
+      "Benjamin Franklin",
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "Benjamin Franklin",
+    questionId: "3182461"
+  },
+  {
+    question: "Que signifient les lettres du fuseau horaire GMT?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Lequel de ceux-ci n'est pas une conception d'épée européenne typique?",
+    answers: ["Cimeterre", "Fauchon", "Ulfberht", "Flamberge"],
+    correct: "Cimeterre",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'Selon Sherlock Holmes, "Si vous éliminez l impossible, tout ce qui reste, aussi improbable soit-il, doit être le ..."',
+    answers: ["Vérité", "Réponse", "Cause", "Source"],
+    correct: "Vérité",
+    questionId: "3410327"
+  },
+  {
+    question: "Quel est le nom de la Pologne en polonais?",
+    answers: ["Polska", "Pupcia", "Polszka", "Paland"],
+    correct: "Polska",
+    questionId: "3542596"
+  },
+  {
+    question: "Le slogan du New York Times est: «Toutes les nouvelles qui conviennent à…»",
+    answers: ["Impression", "Digérer", "Regardeer", "Lire"],
+    correct: "Impression",
+    questionId: "3667517"
+  },
+  {
+    question: "Que signifient les lettres de la chaîne de restauration rapide KFC?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Quelle mascotte de restaurant est un clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'De quelle couleur est le «Ex» dans FedEx Ground?',
+    answers: ["Vert", "Rouge", "Bleu clair", "Orange"],
+    correct: "Vert",
+    questionId: "3913430"
+  },
+  {
+    question: "Quelle est la hauteur du Burj Khalifa?",
+    answers: ["82,96656 cm", "82,81416 cm", "77,60208 cm", "92,17152 cm"],
+    correct: "82,96656 cm",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Lequel des jeux de cartes suivants tourne autour des nombres et des mathématiques de base?",
+    answers: ["Uno", "Go Fish", "Twister", "Munchkin"],
+    correct: "Uno",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Quel élément de la machine est situé au centre des spinners fidget?",
+    answers: ["Roulements", "Essieux", "Engrenages", "Ceintures"],
+    correct: "Roulements",
+    questionId: "4235063"
+  },
+  {
+    question: "Quel signe du zodiaque se situe entre la Vierge et le Scorpion?",
+    answers: ["Balance", "Gémeaux", "Taureau", "Capricorne"],
+    correct: "Balance",
+    questionId: "4321002"
+  },
+  {
+    question: "Lequel des présidents suivants n'est pas sur le mont Rushmore?",
+    answers: [
+      "John F. Kennedy",
+      "Theodore Roosevelt",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "John F. Kennedy",
+    questionId: "4442286"
+  },
+  {
+    question: "Qu'est-ce que la Tasmanie?",
+    answers: [
+      "Un État australien",
+      "Une saveur de glace Ben and Jerry's",
+      "Un trouble psychologique",
+      "Le nom d'un personnage de dessin animé Warner Bross"
+    ],
+    correct: "Un État australien",
+    questionId: "4564430"
+  },
+  {
+    question: "Quelle entreprise a développé le vocaloid Hatsune Miku?",
+    answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
+    correct: "Crypton Future Media",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Quel pays, non compris le Japon, a le plus de ressortissant japonais?",
+    answers: ["Brézil", "Chine", "Corée du Sud", "USA"],
+    correct: "Brézil",
+    questionId: "4795960"
+  },
+  {
+    question: "Quel bonbon n'est PAS fabriqué par Mars?",
+    answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
+    correct: "Almond Joy",
+    questionId: "4811162"
+  },
+  {
+    question: "Dans quelle chaîne de restauration rapide pouvez-vous commander un Jamocha Shake?",
+    answers: ["Arby's", "McDonald's", "Burger King", "Wendy's"],
+    correct: "Arby's",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
